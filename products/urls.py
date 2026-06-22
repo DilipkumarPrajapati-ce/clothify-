45040b9d2e9add6cac491dc02d071cb22a9426df
Dilipkumar prajapati <dilipkumarprajapati.ce@gmail.com>
2026-06-22 19:27:29 +0530

    day 2
    work to clothing web

--- products/urls.py
+++ products/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage , name='home'),
-    path('home', views.homepage , name='home'),
-
+   
+   
+    path('', views.home, name='home'),
+    path('men/', views.men, name='men'),
+    path('women/', views.women, name='women'),
+    path('kids/', views.kids, name='kids'),
+    path('cart/', views.cart, name='cart'),
+    path('login/', views.login, name='login'),
+    
 
 ]
--- products/views.py
+++ products/views.py
@@ -4,5 +4,22 @@ from django.http import HttpResponse
 # Create your views here.
 # Create your views here.
 
-def homepage(request):
-    return render(request, "home.html")
+
+
+def home(request):
+    return render(request,'home.html')
+
+def men(request):
+    return render(request,'men.html')
+
+def women(request):
+    return render(request,'women.html')
+
+def kids(request):
+    return render(request,'kids.html')
+
+def cart(request):
+    return render(request,'cart.html')
+
+def login(request):
+    return render(request,'login.html')
